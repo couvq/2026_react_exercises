2aa6e2f6c463436fc34c0d25f99894c961a33a93
couvq <quentincouvelaire@gmail.com>
2026-08-19 18:35:43 -0400

    show columns and cards with data and basic styling

---
+++ src/examples/kanban/KanbanContent.tsx
@@ -0,0 +1,25 @@
+import Column from "./column"
+import Header from "./Header"
+import { useKanban } from "./KanbanContext";
+
+interface KanbanContentProps {
+    headerTitle: string,
+}
+
+const KanbanContent = ({ headerTitle }: KanbanContentProps) => {
+    const { columns } = useKanban();
+    const flattenedColumns = [...columns.entries()].map(([title, column]) => ({ title, ...column }));
+
+    return (
+        <>
+            <Header title={headerTitle} />
+            <div className="kanban_board">
+                {flattenedColumns.map((column) => (
+                    <Column key={column.title} columnTitle={column.title} cards={column.cards} />
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default KanbanContent
